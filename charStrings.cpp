0fcb313cc30ecafa174bf7656432239bb30de10f
gmear <guillaume.mear2712@gmail.com>
2020-07-18 10:17:32 +0200

    Creation charStrings : classe de test sur les chaines de characteres/strings

---
+++ charStrings.cpp
@@ -0,0 +1,30 @@
+/**
+ * @Author: MEAR Guillaume <Lhyos>
+ * @Date:   18-Jul-2020
+ * @Email:  guillaume.mear2712@gmail.com
+ * @Filename: charStrings.cpp
+ * @Last modified by:   Lhyos
+ * @Last modified time: 18-Jul-2020
+ */
+
+
+
+#include <cstdio>
+
+using namespace std;
+
+
+void testChar();
+
+int main(int argc, char const *argv[]) {
+
+  printf("Cette classe sert a tester l'usage des char et des strings \n");
+
+  testChar();
+  return 0;
+}
+
+void testChar()
+{
+  printf("test fonction \n");
+}
